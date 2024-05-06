8f1900b4b55b13e8e0d13876df03ec56ab6f6678
alendavies <alendavies10@gmail.com>
2024-05-06 18:31:44 -0300

    handle upload

--- src/lib/FileService.py
+++ src/lib/FileService.py
@@ -1,9 +1,14 @@
+from lib.loggers.BaseLogger import BaseLogger
+
+
 class FileService:
-    
+
     def __init__(self, path: str):
         self.path = path
+        self.logger = BaseLogger("FileService")
 
     def save_file(self, filename: str, data: bytes):
+        self.logger.debug("Saving file: " + filename)
         file = open(self.path + filename, "wb")
         file.write(data)
         file.close()
