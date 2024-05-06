3c386e50c42d40895f8bbe22dffecbf5aec38810
lazcanoluca <lazcanoluca@gmail.com>
2024-05-06 18:32:20 -0300

    sr

--- src/lib/protocols/selective_repeat/Client.py
+++ src/lib/protocols/selective_repeat/Client.py
@@ -11,6 +11,7 @@ import time
 class Client(ProtocolClient):
 
     WINDOW_SIZE = 10
+    PACKET_TIMEOUT = 1
 
     def __init__(self, connection: Connection, file_service: FileService):
         super().__init__(connection, file_service)
@@ -34,22 +35,57 @@ class Client(ProtocolClient):
         in_flight: list[tuple[DataPacket, float]] = []
         base = 1 # first block in the window
         next = 1 # next block to be sent
+        last = len(data) // PACKET_SIZE + 1
+        print(last)
+        sent_last = False
 
         # TODO: se puede hacer más elegante lo del timestamp
         # si el data packet tiene un atributo timestamp
         # y cuando se envía, se le setea sólo... pienso...
 
-        while True:
+        while len(in_flight) > 0 or next <= last:
             # Mando paquetes hasta llenar la ventana.
-            while next - base < self.WINDOW_SIZE:
+            while next - base + 1 < self.WINDOW_SIZE and not sent_last:
                 offset = (next-1) * PACKET_SIZE
-                chunk = data[offset:offset+PACKET_SIZE]
+
+                # El contenido del paquete es PACKET_SIZE,
+                # o menos si es el último.
+                if len(data[offset:]) < PACKET_SIZE:
+                    chunk = data[offset:]
+                else:
+                    chunk = data[offset:offset+PACKET_SIZE]
 
                 data_packet = DataPacket(next, chunk)
                 self.connection.send(data_packet)
 
                 in_flight.append((data_packet, time.time()))
+                
+                # Si acabo de mandar el último, no aumento más la ventana.
+                # No se van a mandar más paquetes.
+                if data_packet.is_final_packet():
+                    sent_last = True
                 next += 1
+
+            for packet, timestamp in in_flight:
+                if time.time() - timestamp > self.PACKET_TIMEOUT:
+                    self.connection.send(packet)
+                    timestamp = time.time()
+
+            try:
+                packet, _ = self.connection.receive()
+            except Exception:
+                continue
+            
+            if isinstance(packet, AckPacket):
+                # Saco el paquete acknowledged the in_flight.
+                for i in range(len(in_flight)):
+                    if in_flight[i][0].get_block_number() == packet.get_block_number():
+                        in_flight.pop(i)
+                        break
+                # Si el paquete acknowledgeado es el base, actualizo la base al primero in_flight.
+                if packet.get_block_number() == base:
+                    base = in_flight[0][0].get_block_number()
+            
 
 
         # transmit file
