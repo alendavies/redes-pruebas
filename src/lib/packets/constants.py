bb4783c9541bc5a8f69e42ea1f95240d8f47ef42
lazcanoluca <lazcanoluca@gmail.com>
2024-05-07 16:18:36 -0300

    mjemf

--- src/lib/packets/constants.py
+++ src/lib/packets/constants.py
@@ -1,12 +1,7 @@
 from enum import IntEnum
 
-<<<<<<< Updated upstream
 PACKET_SIZE = 512
 MAX_ATTEMPTS = 3
-=======
-# PACKET_SIZE = 512
-PACKET_SIZE = 5
->>>>>>> Stashed changes
 
 
 class Type(IntEnum):
